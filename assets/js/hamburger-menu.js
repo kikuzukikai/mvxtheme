20250722cd085caeb22e657af1625a9cc9e94949
堀江仁貴 <32335980+HorieHitoki@users.noreply.github.com>
2020-01-08 14:45:40 +0900

    Delete hamburger-menu.js

--- assets/js/hamburger-menu.js
+++
@@ -1,2 +0,0 @@
-/* Copyright (c) 2017 MvvmCross */var hamburgerMenu=openMenu("hamburger-menu");function openMenu(id){var element=document.getElementById(id);return function(){with(element.style){display=(display==="block")?"none":"block";}};}
-$(document).click(function(){$('#hamburger-menu').hide();});
