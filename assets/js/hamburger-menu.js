aed0f485ec5a1bd47744df6454c4514fb816975f
堀江仁貴 <32335980+HorieHitoki@users.noreply.github.com>
2020-01-08 13:41:22 +0900

    Update hamburger-menu.js

--- assets/js/hamburger-menu.js
+++ assets/js/hamburger-menu.js
@@ -1,2 +1,2 @@
 /* Copyright (c) 2017 MvvmCross */var hamburgerMenu=openMenu("hamburger-menu");function openMenu(id){var element=document.getElementById(id);return function(){with(element.style){left=(left==="0px")?"-284px":"0px";}};}
-$(document).click(function(){$('#hamburger-menu').toggle(flip++%2==1);});
+$(document).click(function(){$('#hamburger-menu').toggle(flip++%2==0);});
