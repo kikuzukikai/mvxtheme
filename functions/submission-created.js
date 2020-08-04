cfbeeeb402ffee97a2d7e00927808d96846b53f6
堀江仁貴 <32335980+HorieHitoki@users.noreply.github.com>
2020-08-05 01:44:41 +0900

    Update and rename sendMail.js to submission-created.js

---
+++ functions/submission-created.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
+exports.handler = function(event, context, callback) {
+  const { name, email, message } = JSON.parse(event.body).payload.data
+
+  console.log(user);
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_LOGIN,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN
+    },
+  });
+
+  console.log(event.body);
+
+  let mailOptions = {
+    from: process.env.MAIL_LOGIN,
+    to: '${email}',
+    subject: 'お問い合わせありがとうございます',
+    text: event.body,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: 'Ok',
+      });
+    }
+  });
+};
