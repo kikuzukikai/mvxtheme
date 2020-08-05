97048d6160c40f418383f2cba3c0b28bbca33608
堀江仁貴 <32335980+HorieHitoki@users.noreply.github.com>
2020-08-05 10:53:18 +0900

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,31 +3,33 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 exports.handler = function(event, context, callback) {
-  const { message, email } = JSON.parse(event.body).payload.data
+  const { email, message } = JSON.parse(event.body).payload.data;
 
-  console.log(user);
+  // OAuth認証情報
+  const auth = {
+    type         : 'OAuth2',
+    user         : process.env.MAIL_LOGIN,
+    clientId     : process.env.CLIENT_ID,
+    clientSecret : process.env.CLIENT_SECRET,
+    refreshToken : process.env.REFRESH_TOKEN
+  };
 
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      type: 'OAuth2',
-      user: process.env.MAIL_LOGIN,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: process.env.ACCESS_TOKEN
-    },
-  });
 
-  console.log(event.body);
+  // トランスポート
+  const transport = {
+    service : 'gmail',
+    auth    : auth
+  };
+
+  let transporter = nodemailer.createTransport(transport);
+
+  const url = 'https://www.kikuzukikai.org/';
 
   let mailOptions = {
-    from: process.env.MAIL_LOGIN,
-    to: '${email}',
-    subject: 'お問い合わせありがとうございます',
-    text: event.body
+    from    : `process.env.MAIL_LOGIN`,
+    to      : `${email}`,
+    subject : '【駆逐艦菊月会】お問い合わせありがとうございます',
+    text    : `お問い合わせありがとうございます。\n以下の内容でフォームを送信いたしました。\n数日中に追って担当者よりメールにて回答をお送りいたします。\n今しばらくお待ちください。\n\n------ 送信内容 ------\n【メールアドレス】\n${email}\n\n【お問い合わせ内容】\n${message}\n\n--------------------\n駆逐艦菊月会\n${url}\n`,
   };
 
   transporter.sendMail(mailOptions, function(error, info) {
@@ -36,7 +38,7 @@ exports.handler = function(event, context, callback) {
     } else {
       callback(null, {
         statusCode: 200,
-        body: 'Ok',
+        body: 'ok',
       });
     }
   });
