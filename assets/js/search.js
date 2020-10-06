ca8e4698776e8836885db48aa71f70b0ef526bf5
堀江仁貴 <32335980+HorieHitoki@users.noreply.github.com>
2020-10-06 20:35:49 +0900

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,66 +1,57 @@
-var lunrIndex, $results, pagesIndex;
+jQuery(function() {
+  // Initalize lunr with the fields it will be searching on. I've given title
+  // a boost of 10 to indicate matches on this field are more important.
+  window.idx = lunr(function () {
+    this.field('id');
+    this.field('title', { boost: 10 });
+    this.field('author');
+    this.field('category');
+    this.use(lunr.ja);
+  });
 
-function initLunr() {
-  $.getJSON("search_data.json").done(function(index) {
-      pagesIndex = index;
-      lunrIndex = lunr(function() {
-        var lunrConfig = this;
-        lunrConfig.use(lunr.multiLanguage('en', 'ja'));
-        lunrConfig.ref("href");
-        lunrConfig.field("title", { boost: 10 });
-        lunrConfig.field("contents");
-        pagesIndex.forEach(function(page) {
-          lunrConfig.add(page);
+  // Download the data from the JSON file we generated
+  window.data = $.getJSON('/search_data.json');
+
+  // Wait for the data to load and add it to lunr
+  window.data.then(function(loaded_data){
+    $.each(loaded_data, function(index, value){
+      window.idx.add(
+        $.extend({ "id": index }, value)
+      );
+    });
+  });
+
+  // Event when the form is submitted
+  $("#site_search").submit(function(){
+      event.preventDefault();
+      var query = $("#search_box").val(); // Get the value for the text field
+      var results = window.idx.search(query); // Get lunr to perform a search
+      display_search_results(results); // Hand the results off to be displayed
+  });
+
+  function display_search_results(results) {
+    var $search_results = $("#search_results");
+
+    // Wait for data to load
+    window.data.then(function(loaded_data) {
+
+      // Are there any results?
+      if (results.length) {
+        $search_results.empty(); // Clear any old results
+
+        // Iterate over the results
+        results.forEach(function(result) {
+          var item = loaded_data[result.ref];
+
+          // Build a snippet of HTML for this result
+          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+
+          // Add it to the results
+          $search_results.append(appendString);
         });
-      });
-    })
-  .fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.error("Error getting Jekyll index flie:", err);
-  });
-}
-
-function search(){
-  $results = $("#results");
-  $results.empty();
-  var query = document.getElementById('search-query').value;
-  if (query.length < 2) {
-    return;
+      } else {
+        $search_results.html('<li>No results found</li>');
+      }
+    });
   }
-  renderResults(results(query));
-}
-
-function results(query) {
-  return lunrIndex.search(`*${query}*`).map(function(result) {
-    return pagesIndex.filter(function(page) {
-      return page.href === result.ref;
-    })[0];
-  });
-}
-
-function renderResults(results) {
-  if (!results.length) {
-    $results.append('<p>No matches found</p>');
-    return;
-  }
-
-  results.forEach(function(result) {
-    var $result = $("<li>");
-    $result.append($("<a>", {
-      href: result.href,
-      text: result.title
-    }));
-    if (result.contents.length <= 100) {
-      $result.append($("<p>", {
-        text: result.contents
-      }));
-    } else {
-      $result.append($("<p>", {
-        text: result.contents.slice(0, 100) + " ..."
-      }));
-    }
-    $results.append($result);
-  });
-}
-
-initLunr();
+});
